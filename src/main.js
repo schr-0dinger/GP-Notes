bbb12271aa00cfe84a186bd92b804a876449e001
schr-0dinger <kmkailasnath@gmail.com>
2023-06-27 11:31:43 +0530

    Improved Interface, database and search logic

--- src/main.js
+++ src/main.js
@@ -1,30 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Event listener for search bar
-    document.getElementById('searchbar').addEventListener('keyup', fetchData);
-  });
-  
-  // Function to fetch data from JSON file
-  function fetchData() {
-    fetch('src/data.json')  // Replace 'data.json' with the actual path to your JSON file
-      .then(response => response.json())
-      .then(data => {
-        const searchQuery = document.getElementById('searchbar').value.toLowerCase();
-        
-        // Filtering the data based on the search query
-        const filteredData = data.antibiotic.filter(item => item.dose.toLowerCase().includes(searchQuery));
-        
-        // Displaying the filtered data
-        const resultContainer = document.getElementById('result');
-        resultContainer.innerHTML = '';
-  
-        filteredData.forEach(item => {
-          const listItem = document.createElement('li');
-          listItem.textContent = item.dose;
-          resultContainer.appendChild(listItem);
-        });
-      })
-      .catch(error => {
-        console.log('Error fetching data:', error);
-      });
-  }
-  
+  // Event listener for search bar
+  document.getElementById('searchbar').addEventListener('keyup', fetchData);
+});
+
+// Function to fetch data from JSON file
+function fetchData() {
+  fetch('src/data.json') 
+    .then(response => response.json())
+    .then(database => {
+
+        var input = document.getElementById('searchbar').value.toLowerCase();
+        var resultDiv = document.getElementById('resultbox');
+        resultDiv.innerHTML = ''; // Clear previous results
+    
+        for (var i = 0; i < database.symptom.length; i++) {
+          if (database.symptom[i].disease !== undefined) {
+            var disease = database.symptom[i].disease.toLowerCase();
+            console.log(disease);
+          } else {
+            console.log("Disease property not found or undefined.");
+          }
+            if(input === '' || input.length === 1){
+              resultDiv.innerHTML = '';
+            }
+            else{
+    
+            if (disease.includes(input)) {
+                // Format the result as you wish
+                var result = '<p> Disease: '     + database.symptom[i].disease + '</p>' +
+                             '<p> Drug: ' + database.symptom[i].drugs[0].name + '</p>';
+                                // ' Treatment: ' + database.symptom[i].treatment + 
+                              
+                resultDiv.innerHTML += result;
+            }
+          }
+        }
+    })
+    .catch(error => {
+      console.log('Error fetching data:', error);
+    });
+}

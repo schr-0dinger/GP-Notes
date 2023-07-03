d352a75b46b38f81cb8cdc7428993dcb5c3f244a
schr-0dinger <kmkailasnath@gmail.com>
2023-07-03 13:32:39 +0530

    Simplify code for fetching and displaying search results
    This commit simplifies the code responsible for fetching data from a JSON file and displaying search results. The changes include:
    
    Refactoring the event listener to use arrow functions for concise syntax.
    Utilizing destructuring assignment to extract values from the database.symptom[i] object.
    Replacing the for loop with functional array methods like forEach, filter, and map.
    Removing unnecessary variables and simplifying the logic.
    Utilizing template literals to improve the readability of HTML string generation.
    These modifications improve code readability, reduce complexity, and follow best practices for modern JavaScript development.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   // Event listener for search bar
   document.getElementById('searchbar').addEventListener('keyup', fetchData);
 });
@@ -8,106 +8,80 @@ function fetchData() {
   fetch('src/data.json')
     .then(response => response.json())
     .then(database => {
-
-      var input = document.getElementById('searchbar').value.toLowerCase();
-      var resultDiv = document.getElementById('resultbox');
+      const input = document.getElementById('searchbar').value.toLowerCase();
+      const resultDiv = document.getElementById('resultbox');
       resultDiv.innerHTML = ''; // Clear previous results
 
-      var [drugGroup, drugName, drugList] = [[], [], []];
+      database.symptom.forEach(symptom => {
+        if (symptom.disease) {
+          const disease = symptom.disease.toLowerCase();
 
-      for (var i = 0; i < database.symptom.length; i++) {
-        if (database.symptom[i].disease !== undefined) {
-          var disease = database.symptom[i].disease.toLowerCase();
-          console.log(disease);
-        } else {
-          console.log("Disease property not found or undefined.");
-        }
+          // Results are formatted here
+          if (input === '' || input.length === 1) {
+            resultDiv.innerHTML = '';
+          } else {
+            if (disease.includes(input)) {
+              const drugList = symptom.drugs
+                ? symptom.drugs
+                    .map(drug => drug.generic)
+                    .filter((drug, index, array) => array.indexOf(drug) === index)
+                    .map(
+                      drugfn => `
+                        <div class="list-group">
+                          <a href="#" class="format transparent list-group-item list-group-item-action">
+                            <p>${drugfn}</p>
+                          </a>
+                        </div>
+                      `
+                    )
+                    .join('')
+                : '';
 
-        // Results are formatted here
-        if (input === '' || input.length === 1) {
-          resultDiv.innerHTML = '';
-        }
-        else {
-
-          if (disease.indexOf(input) !== -1) {
-
-            // resultDiv.innerHTML += '<a href=# class="list-group-item">' + database.symptom[i].disease.toUpperCase() + '</a>';
-            if (database.symptom[i].drugs) {
-              var n = database.symptom[i].drugs.length;
-              for (var j = 0; j < n; j++) {
-                var drugType = database.symptom[i].drugs[j].type[0];
-                var isDuplicate = false;
-
-                for (let k = 0; k < drugGroup.length; k++) {
-                  if (drugGroup[k] === drugType) {
-                    isDuplicate = true;
-                    break;
-                  }
-                }
-
-                if (!isDuplicate) {
-                  drugGroup.push(drugType);
-                }
-                drugName.push(database.symptom[i].drugs[j].generic);
-
-                var uniqueDrugName = [...new Set(drugName)];
-
-                var drugList = uniqueDrugName.map(function(drugfn) {
-                  return '<div class="list-group"><a href="#" class="format transparent list-group-item list-group-item-action">' +
-                    '<p>' + drugfn + '</p></a></div>';
-                });
-                            
-              
-                // console.log('New Array is: ' + drugGroup + ' ' + Array.isArray(drugGroup));
-
-                //Design a dynamic card collection that shows the result in a systematic way.
-
-                var result = `
+              const result = `
                 <div id="accordion"> 
-<div class="card glass format">
-    <div class="card-header">
-        <a id="resultbox" class="collapsed btn" data-bs-toggle="collapse" href="#${database.symptom[i].disease.toLowerCase().replace(/\s/g, "").replace(/\//g, "")}Id">
-        ${database.symptom[i].disease.toUpperCase()}
-        </a>
-    </div>
-    <div id="${database.symptom[i].disease.toLowerCase().replace(/\s/g, "").replace(/\//g, "")}Id" class="collapse" data-bs-parent="#accordion">
-        <div class="card-body">
-            <ul class="nav nav-pills justify-content-evenly" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" data-bs-toggle="pill" href="#treatment">Treatment</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="pill" href="#drug">Drugs</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="pill" href="#pediatricdose">Pediatric dose</a>
-                </li>
-            </ul>
-            <div class="tab-content">
-                <div id="treatment" class="container tab-pane active"><br>
-                    ${database.symptom[i].treatment}
+                  <div class="card glass format">
+                    <div class="card-header">
+                      <a id="resultbox" class="collapsed btn" data-bs-toggle="collapse" href="#${disease.replace(/\s/g, '').replace(/\//g, '')}Id">
+                        ${symptom.disease.toUpperCase()}
+                      </a>
+                    </div>
+                    <div id="${disease.replace(/\s/g, '').replace(/\//g, '')}Id" class="collapse" data-bs-parent="#accordion">
+                      <div class="card-body">
+                        <ul class="nav nav-pills justify-content-evenly" role="tablist">
+                          <li class="nav-item">
+                            <a class="nav-link active" data-bs-toggle="pill" href="#treatment">Treatment</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="pill" href="#drug">Drugs</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="pill" href="#pediatricdose">Pediatric dose</a>
+                          </li>
+                        </ul>
+                        <div class="tab-content">
+                          <div id="treatment" class="container tab-pane active"><br>
+                            ${symptom.treatment}
+                          </div>
+                          <div id="drug" class="container tab-pane fade"><br>
+                            ${drugList}
+                          </div>
+                          <div id="pediatricdose" class="container tab-pane fade"><br>
+                            ${symptom.drugs && symptom.drugs[0].pediatric_dose}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div id="drug" class="container tab-pane fade"><br>
-                ${drugList.join('')}
-                </div>
-                <div id="pediatricdose" class="container tab-pane fade"><br>
-                    ${database.symptom[i].drugs[j].pediatric_dose}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>   
-</div>    
-`;
+              `;
 
-              }
+              resultDiv.innerHTML += result;
             }
-            console.log('Unique drug array: ' + drugList + ' ' + Array.isArray(uniqueDrugName))
-            resultDiv.innerHTML += result;
           }
-
+        } else {
+          console.log('Disease property not found or undefined.');
         }
-      }
+      });
     })
     .catch(error => {
       console.log('Error fetching data:', error);

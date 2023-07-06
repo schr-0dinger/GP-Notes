9f0061c6654fa0bde25d15ca267eec6bf228cb81
schr-0dinger <kmkailasnath@gmail.com>
2023-07-06 18:16:30 +0530

    Major changes
    Fixed drug list accordion display
    Added some fa icons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Event listener for search bar
   document.getElementById("searchbar").addEventListener("keyup", fetchData);
 });
+
 // Function to fetch data from JSON file
 function fetchData() {
   fetch("src/data.json")
@@ -11,28 +12,20 @@ function fetchData() {
       var resultDiv = document.getElementById("resultbox");
       resultDiv.innerHTML = ""; // Clear previous results
 
-      var [drugGroup, drugName, drugList] = [[], [], []];
-
       for (var i = 0; i < database.symptom.length; i++) {
-        if (database.symptom[i].disease !== undefined) {
-          var disease = database.symptom[i].disease.toLowerCase();
-        } else {
-          console.log("Disease property not found or undefined.");
-        }
+        var disease = database.symptom[i].disease?.toLowerCase(); // Use optional chaining to handle undefined disease property
 
         // Results are formatted here
         if (input === "" || input.length === 1) {
           resultDiv.innerHTML = "";
         } else {
-          if (disease.indexOf(input) !== -1) {
-            // true if the value of "input" is found in the "disease" array or string, and false if it is not found.
+          if (disease && disease.includes(input)) { // Use includes() method to check if input is found in disease
             if (database.symptom[i].drugs) {
-              var n = database.symptom[i].drugs.length;
               var drugTypeAccordion = {}; // Object to store drug types and corresponding drug names
 
-              for (var j = 0; j < n; j++) {
-                var drugType = database.symptom[i].drugs[j].type[0];
-                var drugName = database.symptom[i].drugs[j].generic;
+              database.symptom[i].drugs.forEach((drug) => {
+                var drugType = drug.type[0];
+                var drugName = drug.generic;
 
                 // Check if drug type already exists in the accordion object
                 if (drugType in drugTypeAccordion) {
@@ -40,7 +33,7 @@ function fetchData() {
                 } else {
                   drugTypeAccordion[drugType] = [drugName];
                 }
-              }
+              });
 
               // Generate accordion HTML for each drug type
               var accordionHTML = "";
@@ -49,30 +42,17 @@ function fetchData() {
 
                 var drugListHTML = drugNames
                   .map(function (drugName) {
-                    return (
-                      '<a href="#" class="format transparent list-group-item list-group-item-action">' +
-                      "<p>" +
-                      drugName +
-                      "</p></a>"
-                    );
+                    return `<a href="#" class="transparent white list-group-item">${drugName}</a>`;
                   })
                   .join("");
 
                 accordionHTML += `
-                  <div class="accordion-item glass">
-                    <div class="accordion-header glass">
-                      <button class="accordion-button glass" data-bs-toggle="collapse" data-bs-target="#collapse${type
-                        .replace(/\s/g, "")
-                        .replace(/\//g, "")}">
-                        ${type}
-                      </button>
+                  <div class="accordion-item transparent white">
+                    <div class="accordion-header">
+                      <button class="accordion-button transparent white" data-bs-toggle="collapse" data-bs-target="#collapse${type.replace(
+                        /[\s\/\(\)\&\+]/g,"")}"><i class="fa-solid fa-pills fa-2xs"></i> &#160;${type[0].toUpperCase() + type.substring(1)}</button>
                     </div>
-                    <div id="collapse${type
-                      .replace(/\s/g, "")
-                      .replace(
-                        /\//g,
-                        ""
-                      )}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                    <div id="collapse${type.replace(/[\s\/\(\)\&\+]/g,"")}" class="accordion-collapse collapse glass2" data-bs-parent="#accordionExample">
                       <div class="accordion-body">
                         <div class="list-group">
                           ${drugListHTML}
@@ -81,71 +61,57 @@ function fetchData() {
                     </div>
                   </div>
                 `;
-
-                // Insert the accordion HTML into the desired container element
-
-                var result = `
-                <div id="accordion${i}"> 
-<div class="card glass format">
-    <div class="card-header">
-        <a id="resultbox" class="collapsed btn" data-bs-toggle="collapse" href="#${database.symptom[
-          i
-        ].disease
-          .toLowerCase()
-          .replace(/\s/g, "")
-          .replace(/\//g, "")}Id">
-        ${database.symptom[i].disease.toUpperCase()}
-        </a>
-    </div>
-    <div id="${database.symptom[i].disease
-      .toLowerCase()
-      .replace(/\s/g, "")
-      .replace(/\//g, "")}Id" class="collapse" data-bs-parent="#accordion${i}">
-        <div class="card-body">
-            <ul class="nav nav-pills justify-content-evenly" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" data-bs-toggle="pill" href="#treatment">Treatment</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="pill" href="#drug">Drugs</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="pill" href="#pediatricdose">Pediatric dose</a>
-                </li>
-            </ul>
-            <div class="tab-content">
-                <div id="treatment" class="container format tab-pane active"><br>
-                <ol>
-                ${database.symptom[i].treatment
-                  .map((item) => `<li>${item}</li>`)
-                  .join("")}
-              </ol>
-                </div>
-                <div id="drug" class="container glass tab-pane fade"><br>
-                <div id="accordionExample">
-                ${accordionHTML}
-                </div>
-                </div>
-                <div id="pediatricdose" class="container tab-pane fade"><br>
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-</div>   
-</div>    
-`;
               }
-            }
 
-            resultDiv.innerHTML += result;
-            // Initialize Bootstrap Collapse component
-            var accordions = document.querySelectorAll(
-              '[data-bs-toggle="collapse"]'
-            );
-            accordions.forEach(function (accordion) {
-              new bootstrap.Collapse(accordion);
-            });
+              // Insert the accordion HTML into the desired container element
+              var result = `
+                <div id="accordion${i}">
+                  <div class="card glass format">
+                    <div class="card-header">
+                      <a id="resultbox" class="collapsed btn" data-bs-toggle="collapse" href="#${disease.replace(
+                        /[\s\/\(\)\&\+]/g,"")}Id"><i class="fa-solid fa-circle-exclamation"></i> ${disease[0].toUpperCase() + disease.substring(1)}</a>
+                    </div>
+                    <div id="${disease.replace(/[\s\/\(\)\&\+]/g,""
+                    )}Id" class="collapse" data-bs-parent="#accordion${i}">
+                      <div class="card-body">
+                        <ul class="nav nav-pills justify-content-evenly" role="tablist">
+                          <li class="nav-item">
+                            <a class="nav-link active" data-bs-toggle="pill" href="#treatment"><i class="fa-solid fa-stethoscope"></i> Treatment</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="pill" href="#drug"><i class="fa-solid fa-prescription"></i> Drugs</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" data-bs-toggle="pill" href="#pediatricdose"><i class="fa-solid fa-baby"></i> Pediatric dose</a>
+                          </li>
+                        </ul>
+                        <div class="tab-content">
+                          <div id="treatment" class="container format tab-pane active"><br>
+                            <ol>
+                              ${database.symptom[i].treatment.map((item) => `<li>${item}</li>`).join("")}
+                            </ol>
+                          </div>
+                          <div id="drug" class="container tab-pane fade"><br>
+                            <div class="accordion accordion-flush" id="accordionExample">
+                              ${accordionHTML}
+                            </div>
+                          </div>
+                          <div id="pediatricdose" class="container tab-pane fade"><br>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              `;
+
+              resultDiv.innerHTML += result;
+              // Initialize Bootstrap Collapse component
+              var accordions = document.querySelectorAll('[data-bs-toggle="collapse"]');
+              accordions.forEach(function (accordion) {
+                new bootstrap.Collapse(accordion);
+              });
+            }
           }
         }
       }
